2585ecbe2627c8f5196e4720ea2b3dbef5255314
Zhenhui-Tian <u6023218@anu.edu.au>
2020-04-14 15:12:30 +1000

    study better solution of 283

--- Week_01/[283]Move Zeroes.java
+++ Week_01/[283]Move Zeroes.java
@@ -18,20 +18,17 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public void moveZeroes(int[] nums) {
-        int temp = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                for (int j = i + 1; j<nums.length; j++) {
-                    if (nums[j] !=0 ){
-                        temp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = temp;
-                        break;
-                    }
-                }
+        int lastZeroFoundAt = 0;
+        for (int i=0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastZeroFoundAt] = nums[i];
+                lastZeroFoundAt ++;
             }
         }
-        
+        for (int i=lastZeroFoundAt; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
